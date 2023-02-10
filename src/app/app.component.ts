915a864b439b0580bd3a6df3685ca7bfb449c26a
LuuLeThaiSon <llts28497@gmail.com>
2023-02-10 17:57:38 +0700

    fix đường dẫn js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
+import {JobService} from "./components/service/job.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
   title = 'find-job-FE';
+  constructor(private elementRef:ElementRef) {
+  };
+
+  ngAfterViewInit() {
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "../../assets/libs/choices.js/public/assets/scripts/choices.min.js";
+    this.elementRef.nativeElement.appendChild(s);
+
+    var s1 = document.createElement("script");
+    s1.type = "text/javascript";
+    s1.src = "../../assets/libs/swiper/swiper-bundle.min.js";
+    this.elementRef.nativeElement.appendChild(s1);
+
+    var s2 = document.createElement("script");
+    s2.type = "text/javascript";
+    s2.src = "../../assets/js/pages/job-list.init.js";
+    this.elementRef.nativeElement.appendChild(s2);
+
+    var s3 = document.createElement("script");
+    s3.type = "text/javascript";
+    s3.src = "../../assets/js/pages/index.init.js";
+    this.elementRef.nativeElement.appendChild(s3);
+
+    var s4 = document.createElement("script");
+    s4.type = "text/javascript";
+    s4.src = "../../assets/js/app.js";
+    this.elementRef.nativeElement.appendChild(s4);
+
+    var s5 = document.createElement("script");
+    s5.type = "text/javascript";
+    s5.src = "../../assets/js/pages/switcher.init.js";
+    this.elementRef.nativeElement.appendChild(s5);
+  }
 }
--- src/app/components/pages/home-page/home-page.component.ts
+++ src/app/components/pages/home-page/home-page.component.ts
@@ -7,44 +7,44 @@ import {Job} from "../../model/job";
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements AfterViewInit{
+export class HomePageComponent{
   jobs: Job[] = [];
-  constructor(private elementRef:ElementRef,
+  constructor(
               private jobService: JobService) {
     this.findAll();
   };
-
-  ngAfterViewInit() {
-    var s = document.createElement("script");
-    s.type = "text/javascript";
-    s.src = "../../assets/libs/choices.js/public/assets/scripts/choices.min.js";
-    this.elementRef.nativeElement.appendChild(s);
-
-    var s1 = document.createElement("script");
-    s1.type = "text/javascript";
-    s1.src = "../../assets/libs/swiper/swiper-bundle.min.js";
-    this.elementRef.nativeElement.appendChild(s1);
-
-    var s2 = document.createElement("script");
-    s2.type = "text/javascript";
-    s2.src = "../../assets/js/pages/job-list.init.js";
-    this.elementRef.nativeElement.appendChild(s2);
-
-    var s3 = document.createElement("script");
-    s3.type = "text/javascript";
-    s3.src = "../../assets/js/pages/index.init.js";
-    this.elementRef.nativeElement.appendChild(s3);
-
-    var s4 = document.createElement("script");
-    s4.type = "text/javascript";
-    s4.src = "../../assets/js/app.js";
-    this.elementRef.nativeElement.appendChild(s4);
-
-    var s5 = document.createElement("script");
-    s5.type = "text/javascript";
-    s5.src = "../../assets/js/pages/switcher.init.js";
-    this.elementRef.nativeElement.appendChild(s5);
-  }
+  //
+  // ngAfterViewInit() {
+  //   var s = document.createElement("script");
+  //   s.type = "text/javascript";
+  //   s.src = "../../assets/libs/choices.js/public/assets/scripts/choices.min.js";
+  //   this.elementRef.nativeElement.appendChild(s);
+  //
+  //   var s1 = document.createElement("script");
+  //   s1.type = "text/javascript";
+  //   s1.src = "../../assets/libs/swiper/swiper-bundle.min.js";
+  //   this.elementRef.nativeElement.appendChild(s1);
+  //
+  //   var s2 = document.createElement("script");
+  //   s2.type = "text/javascript";
+  //   s2.src = "../../assets/js/pages/job-list.init.js";
+  //   this.elementRef.nativeElement.appendChild(s2);
+  //
+  //   var s3 = document.createElement("script");
+  //   s3.type = "text/javascript";
+  //   s3.src = "../../assets/js/pages/index.init.js";
+  //   this.elementRef.nativeElement.appendChild(s3);
+  //
+  //   var s4 = document.createElement("script");
+  //   s4.type = "text/javascript";
+  //   s4.src = "../../assets/js/app.js";
+  //   this.elementRef.nativeElement.appendChild(s4);
+  //
+  //   var s5 = document.createElement("script");
+  //   s5.type = "text/javascript";
+  //   s5.src = "../../assets/js/pages/switcher.init.js";
+  //   this.elementRef.nativeElement.appendChild(s5);
+  // }
 
   findAll() {
     return this.jobService.findAll().subscribe((data) => {
