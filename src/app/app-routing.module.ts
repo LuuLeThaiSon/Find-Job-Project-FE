b2e340729f3ec93ad559ba0b87d4c233729ff061
HungVo19 <vohung193@gmail.com>
2023-02-13 09:40:07 +0700

    update router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomePageComponent} from "./components/pages/home-page/home-page.component";
 import {ContactComponent} from "./components/pages/contact/contact.component";
 import {BlogComponent} from "./components/pages/blog/blog.component";
+import {CompanyListComponent} from "./components/pages/company-list/company-list.component";
+import {JobListComponent} from "./components/pages/job-list/job-list.component";
 
 const routes: Routes = [
   {path:"",component:HomePageComponent},
   {path:"contact",component:ContactComponent},
-  {path:"blog",component:BlogComponent}
+  {path:"blog",component:BlogComponent},
+  {path:"company",component:CompanyListComponent},
+  {path:"job",component:JobListComponent}
 ];
 
 @NgModule({
