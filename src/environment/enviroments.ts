2eec24f56686c67862bd41079e8798aeaf0e72e6
HungVo19 <vohung193@gmail.com>
2023-02-14 11:26:14 +0700

    change company profile layout

--- src/environment/enviroments.ts
+++ src/environment/enviroments.ts
@@ -1,3 +1,12 @@
 export const environments = {
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'http://localhost:8080',
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyAUmYH6iWIwcfUV85s7i0E93hD7Wb-X3YU",
+    authDomain: "findjobspringbootangular.firebaseapp.com",
+    projectId: "findjobspringbootangular",
+    storageBucket: "findjobspringbootangular.appspot.com",
+    messagingSenderId: "681592963283",
+    appId: "1:681592963283:web:15783898346b2be5a0d426"
+  }
 }
