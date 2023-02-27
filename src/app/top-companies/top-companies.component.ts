fc4265aa563523a84fdfe3e8a329e0da99e18752
HungVo19 <vohung193@gmail.com>
2023-02-27 13:46:04 +0700

    sua top companies

--- src/app/top-companies/top-companies.component.ts
+++ src/app/top-companies/top-companies.component.ts
@@ -2,6 +2,8 @@ import {Component} from '@angular/core';
 import {CompanyService} from "../components/service/company.service";
 import {Company} from "../components/model/company";
 import {CommonService} from "../components/service/common.service";
+import {DomSanitizer} from "@angular/platform-browser";
+import {JobService} from "../components/service/job.service";
 
 @Component({
   selector: 'app-top-companies',
@@ -10,14 +12,17 @@ import {CommonService} from "../components/service/common.service";
 })
 export class TopCompaniesComponent {
   companies: Company[] = [];
+  description: any;
+  sumQuantity: number[] = [];
 
   constructor(private companyService: CompanyService,
-              private commonService:CommonService) {
+              private commonService: CommonService,
+              private jobService: JobService) {
   }
 
   ngOnInit() {
-    this.commonService.scrollTopWindow(0,300)
-    this.companyService.findTopCompanies().subscribe(res=>{
+    this.commonService.scrollTopWindow(0, 300)
+    this.companyService.findTopCompanies().subscribe(res => {
       this.companies = res;
     })
   }
